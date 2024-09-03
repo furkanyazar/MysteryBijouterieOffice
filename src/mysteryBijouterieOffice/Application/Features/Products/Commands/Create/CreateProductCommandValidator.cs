a76f3f78e42ddb5898884b9b4dddad7a34ce9ed5
Furkan Yazar <furkanyazar@proton.me>
2024-09-03 15:44:23 +0300

    feat(Application): change validators of create product command and update product command

--- src/mysteryBijouterieOffice/Application/Features/Products/Commands/Create/CreateProductCommandValidator.cs
+++ src/mysteryBijouterieOffice/Application/Features/Products/Commands/Create/CreateProductCommandValidator.cs
@@ -8,25 +8,10 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
     {
         RuleFor(p => p.CategoryId).GreaterThan(0).When(p => p.CategoryId != null);
         RuleFor(p => p.Name).NotEmpty().MinimumLength(2);
-        RuleFor(p => p.BarcodeNumber).NotEmpty().Length(13).Must(StartsWith).Must(Number);
+        RuleFor(p => p.BarcodeNumber).NotEmpty();
         RuleFor(p => p.PurchasePrice).NotNull();
-        RuleFor(p => p.ModelNumber).NotEmpty().Length(8).Must(StartsWith).Must(Number);
+        RuleFor(p => p.ModelNumber).NotEmpty();
         RuleForEach(p => p.ProductMaterials).SetValidator(new CreateProductCommandProductMaterialListItemDtoValidator());
         RuleFor(p => p.UnitsInStock).NotNull();
-    }
-
-    private bool StartsWith(string barcodeNumber) => barcodeNumber.StartsWith("MB-");
-
-    private bool Number(string barcodeNumber)
-    {
-        string[] barcodeSplit = barcodeNumber.Split('-');
-        if (barcodeSplit.Length != 2)
-            return false;
-
-        foreach (var barcode in barcodeSplit[1])
-            if (!char.IsDigit(barcode))
-                return false;
-
-        return true;
     }
 }
--- src/mysteryBijouterieOffice/Application/Features/Products/Commands/Update/UpdateProductCommandValidator.cs
+++ src/mysteryBijouterieOffice/Application/Features/Products/Commands/Update/UpdateProductCommandValidator.cs
@@ -9,25 +9,10 @@ public class UpdateProductCommandValidator : AbstractValidator<UpdateProductComm
         RuleFor(p => p.Id).NotEmpty();
         RuleFor(p => p.CategoryId).GreaterThan(0).When(p => p.CategoryId != null);
         RuleFor(p => p.Name).NotEmpty().MinimumLength(2);
-        RuleFor(p => p.BarcodeNumber).NotEmpty().Length(13).Must(StartsWith).Must(Number);
+        RuleFor(p => p.BarcodeNumber).NotEmpty();
         RuleFor(p => p.PurchasePrice).NotNull();
-        RuleFor(p => p.ModelNumber).NotEmpty().Length(8).Must(StartsWith).Must(Number);
+        RuleFor(p => p.ModelNumber).NotEmpty();
         RuleForEach(p => p.ProductMaterials).SetValidator(new UpdateProductCommandProductMaterialListItemDtoValidator());
         RuleFor(p => p.UnitsInStock).NotNull();
-    }
-
-    private bool StartsWith(string barcodeNumber) => barcodeNumber.StartsWith("MB-");
-
-    private bool Number(string barcodeNumber)
-    {
-        string[] barcodeSplit = barcodeNumber.Split('-');
-        if (barcodeSplit.Length != 2)
-            return false;
-
-        foreach (var barcode in barcodeSplit[1])
-            if (!char.IsDigit(barcode))
-                return false;
-
-        return true;
     }
 }
